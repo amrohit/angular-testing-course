3aefb13889db9bf09f9aa1e74c8157590fcb0d9b
rohitsharmahub <35895869+amrohit@users.noreply.github.com>
2021-10-19 00:11:23 +0530

    Update calculator.service.spec.ts

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -5,11 +5,19 @@ import { LoggerService } from "./logger.service";
 describe("CalculatorService", () => {
   // Specification
   it("should add two number", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    // creating a fake version of logger service by creating a new instance of it
+    const logger = new LoggerService();
+
+    // jasmine will take the original spied object and beside calling the original functionality, it replaces the methods with a mock one instead of using it original one
+    spyOn(logger, "log");
+
+    const calculator = new CalculatorService(logger);
 
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
+
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   // Specification
