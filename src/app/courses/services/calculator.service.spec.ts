2bb9053f368330f8f76411894ec76e20c58bb664
rohitsharmahub <35895869+amrohit@users.noreply.github.com>
2021-10-19 01:08:23 +0530

    Update calculator.service.spec.ts

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,8 +3,25 @@ import { LoggerService } from "./logger.service";
 
 // Test suit
 describe("CalculatorService", () => {
-  // Specification
+  //using beforeEach to avoid code repeating for initialized which we need in more than one
+  //test specification.
+  // beforeEach will run before our specification
+
+  //to make available our variable through out the specs we need to declare here
+
+  let calculator: CalculatorService, loggerSpy: any;
+
+  beforeEach(() => {
+    console.log("calling beforeEach");
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
+  // Specification (and each test are independent and does not interfere with each other)
+  // and the calculation instance that are used on spec 1 is different than the spec 2nd one.
+  // these test should be isolated from each other
   it("should add two number", () => {
+    console.log("calling our first specifications");
     // creating a fake version of logger service by creating a new instance of it
     // const logger = new LoggerService();
 
@@ -12,21 +29,20 @@ describe("CalculatorService", () => {
     // spyOn(logger, "log");
 
     // if we want to do the above steps with  a single line using jasmine.createSpyObj() method then
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    // const logger = jasmine.createSpyObj("LoggerService", ["log"]);
 
-
-    const calculator = new CalculatorService(logger);
+    // const calculator = new CalculatorService(logger);
 
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   // Specification
   it("should subtract two number", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    // const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.subtract(2, 2);
 
