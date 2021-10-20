b86f6a4ab04c94b8a951016dec45dfdf5d7f2e8f
rohitsharmahub <35895869+amrohit@users.noreply.github.com>
2021-10-21 00:55:35 +0530

    Update async-examples.spec.ts

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,4 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
 fdescribe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
@@ -12,4 +13,23 @@ fdescribe("Async Testing Examples", () => {
       done();
     }, 1000);
   });
+
+  it("Asynchronous test example - setTimeout()", fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {});
+
+    setTimeout(() => {
+      console.log("Running assertions");
+
+      test = true;
+
+      expect(test).toBeTruthy();
+    }, 1000);
+
+    // tick(1000); //can be only called inside fakeAsync() block just to forward the time passage by `1000.
+
+    flush();
+    expect(test).toBeTruthy();
+  }));
 });
